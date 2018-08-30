4b9d61252383a6102dec49fcd22e055cb22fa890
kazukitakahasi <kazuki.takahashiyola@gmail.com>
2018-08-30 06:38:52 +0000

    modified HTML,CSS

--- app/controllers/bad_evaluations_controller.rb
+++ app/controllers/bad_evaluations_controller.rb
@@ -1,7 +1,28 @@
 class BadEvaluationsController < ApplicationController
   def create
+  	@user = User.find(params[:user_id])
+    @bad_evaluation = current_user.bad_evaluations.new(evaluated_user: @user.id)
+    @bad_evaluation.save
+
+
+    @good_evaluation = current_user.good_evaluations.find_by(evaluated_user: @user.id)
+    if @good_evaluation.blank?
+    	redirect_to user_path(@user.id)
+    else
+    	@good_evaluation.destroy
+        redirect_to user_path(@user.id)
+    end
   end
 
   def destroy
+  	@user = User.find(params[:user_id])
+    @bad_evaluation = current_user.bad_evaluations.find_by(evaluated_user: @user.id)
+    @bad_evaluation.destroy
+    redirect_to user_path(@user.id)
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:email,:encrypted_password,:name,:profile_image,:gender,:birthday,:pref,:url,:introduction, user_charges_attributes: [:id, :charge_id], user_categories_attributes: [:id, :category_id], favorite_artists_attributes: [:id, :name], favorite_albums_attributes: [:id, :name], favorite_songs_attributes: [:id, :name])
+    end
 end
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,29 @@
 class MessagesController < ApplicationController
   def index
+  	@message = Message.new
+  	@user = User.find(params[:user_id])
+  	@recipient_user = User.find(params[:id])
+  	@messages = Message.where( "(user_id = ? ) OR (user_id = ? )", current_user,@recipient_user.id).where( "(recipient_user = ? ) OR (recipient_user = ? )", current_user,@recipient_user.id )
+  	#binding.pry
+    if @user.id == current_user.id
+      render :index
+    else
+      redirect_to mypage_user_path(current_user)
+    end
   end
 
   def create
+  	@message = Message.new(message_params)
+  	@recipient_user = User.find(params[:id])
+  	@message.user_id = current_user.id
+  	@message.recipient_user = @recipient_user.id
+  	#binding.pry
+  	@message.save
+  	redirect_to user_recipient_user_messages_path(current_user.id, @recipient_user.id)
   end
+
+  private
+    def message_params
+      params.require(:message).permit(:user_id, :content, :recipient_user )
+    end
 end
